159965a74b34ecef2c325a57486811ff43807bba
uexyi <91570411+uexyi@users.noreply.github.com>
2022-04-12 15:04:28 +0200

    Hab was geändert

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -1,5 +1,5 @@
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Bitte wählen Sie ihr");
+        System.out.println("Bitte wählen Sie ihr Getraenk:");
     }
 }
