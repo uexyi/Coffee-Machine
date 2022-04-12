adca96a8a5e47b639e89548bc1509a8bf8f8574e
uexyi <91570411+uexyi@users.noreply.github.com>
2022-04-12 15:07:53 +0200

    Changed to english

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -1,5 +1,5 @@
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Bitte wählen Sie ihr Getraenk:");
+        System.out.println("Please enter a drink:");
     }
 }
